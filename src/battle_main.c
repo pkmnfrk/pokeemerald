7c97e59718ee4e0e57f81171babaaa83db303578
Mike Caron <mike.caron@patientpop.com>
2021-06-30 00:18:48 -0400

    cleanup + add the randomized pokemon to the pokedex

--- src/battle_main.c
+++ src/battle_main.c
@@ -5186,15 +5186,31 @@ void RunBattleScriptCommands(void)
 void RandomizePartyPokemon(struct Pokemon *party)
 {
     struct Pokemon *mon;
-    u16 i, rnd, oldSpecies;
+    u16 i, rnd, oldSpecies, tmp = 0, personality;
     for(i = 0; i < PARTY_SIZE; ++i) {
         mon = &party[i];
         if(!(oldSpecies = GetMonData(mon, MON_DATA_SPECIES, 0)) || GetMonData(mon, MON_DATA_IS_EGG, 0)) {
             continue;
         }
-        rnd = 1 + Random() % SPECIES_DEOXYS;
+        // first clear existing moves
+        for(rnd = 0; rnd < 4; rnd++) {
+            SetMonData(mon, MON_DATA_MOVE1 + rnd, &tmp);
+        }
+        // set new species
+        // the UNKNOWN_B - Z line is bugged, so skip over them
+        do {
+            rnd = 1 + Random() % SPECIES_DEOXYS;
+        } while(rnd >= SPECIES_OLD_UNOWN_B && rnd <= SPECIES_OLD_UNOWN_Z);
+        
         SetMonData(mon, MON_DATA_SPECIES, &rnd);
+        CalculateMonStats(mon);
+        // give new moves
         GiveMonInitialMoveset(mon);
+        // make sure it's named correctly
         EvolutionRenameMon(mon, oldSpecies, rnd);
+        personality = GetMonData(mon, MON_DATA_PERSONALITY, 0);
+        // add it to the pokedex
+        HandleSetPokedexFlag(SpeciesToNationalPokedexNum(rnd), FLAG_SET_SEEN, personality);
+        HandleSetPokedexFlag(SpeciesToNationalPokedexNum(rnd), FLAG_SET_CAUGHT, personality);
     }
 }
