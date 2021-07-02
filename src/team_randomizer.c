1d7aeabc65cffcae6b603ff5ee1000dadb929fbe
Mike Caron <mike.caron@patientpop.com>
2021-07-02 11:39:44 -0400

    force Mew and Deoxys to work

--- src/team_randomizer.c
+++ src/team_randomizer.c
@@ -9,6 +9,7 @@ void RandomizePartyPokemon(struct Pokemon *party)
 {
     struct Pokemon *mon;
     u16 i, rnd, oldSpecies, tmp = 0, personality;
+    u8 eventLegal = 1;
     for(i = 0; i < PARTY_SIZE; ++i) {
         mon = &party[i];
         if(!(oldSpecies = GetMonData(mon, MON_DATA_SPECIES, 0)) || GetMonData(mon, MON_DATA_IS_EGG, 0)) {
@@ -18,8 +19,7 @@ void RandomizePartyPokemon(struct Pokemon *party)
         for(rnd = 0; rnd < 4; rnd++) {
             SetMonData(mon, MON_DATA_MOVE1 + rnd, &tmp);
         }
-        // set new species
-        // the UNKNOWN_B - Z line is bugged, so skip over them
+        // set new species; the UNKNOWN_B - Z line is bugged, so skip over them
         do {
             rnd = 1 + Random() % SPECIES_DEOXYS;
         } while(rnd >= SPECIES_OLD_UNOWN_B && rnd <= SPECIES_OLD_UNOWN_Z);
@@ -31,6 +31,8 @@ void RandomizePartyPokemon(struct Pokemon *party)
         // make sure it's named correctly
         EvolutionRenameMon(mon, oldSpecies, rnd);
         personality = GetMonData(mon, MON_DATA_PERSONALITY, 0);
+        // Mew and Deoxys will disobey unless this bit is set
+        SetMonData(mon, MON_DATA_EVENT_LEGAL, &eventLegal);
         // add it to the pokedex
         HandleSetPokedexFlag(SpeciesToNationalPokedexNum(rnd), FLAG_SET_SEEN, personality);
         HandleSetPokedexFlag(SpeciesToNationalPokedexNum(rnd), FLAG_SET_CAUGHT, personality);
